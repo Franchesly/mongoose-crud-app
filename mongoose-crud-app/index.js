a25e41a734475df9a129aaa597fdf77447fa7c26
Franchesly <francheslyfe@gmail.com>
2024-12-20 11:37:57 -0500

    changed port to 3000

--- mongoose-crud-app/index.js
+++ mongoose-crud-app/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 const conn = require('./config/db')
 conn()
 const starterFruits = require('./config/seed')
